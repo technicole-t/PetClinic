93b109b33701ac4ed2ae6c9508e723eb25a0c38a
technicole-t <thorleynicole@gmail.com>
2020-06-18 16:50:37 +0100

    able to read first 10 owners, displayed on web browser

--- JS/index.js
+++ JS/index.js
@@ -1,14 +1,28 @@
 const apiUrl = "http://localhost:9966/petclinic/api/";
-
+const cardStyle = "card w-50 border border-indo m5";
 
 function getOwners () {
    // const searchBar = document.getElementById("search")
     fetch(apiUrl + "owners")
     .then(res => res.json())
-    .then(json => console.log(json))
+    .then(json => ownerOutput.innerText = JSON.stringify(json, undefined, 2))
+    .then(json => {
+        const owners = document.getElementById("ownerOutput")
+        const ownersDiv = createElement("h1", "", owners, cardStyle);
+        owners.addEventListener("click", function() {
+            window.location = "../html/index.html";
+        })
+    })
     .catch(err => console.log(err))
 }
 
-// id, firstName, lastName, address, city, pets [], telephone
 
+function createElement(elementType, text, appendTo, className) {
+    const element = document.createElement(elementType);
+    element.innerText = text;
+    appendTo.appendChild(element);
+    element.className = className;
+    return element;
+   }
+// id, firstName, lastName, address, city, pets [], telephone
 // pets is an array of objects - id, name, birthDate,owners, visits [], type{id, name}
